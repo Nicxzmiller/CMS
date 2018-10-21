e91d12413663447e0dc6d708ed3a66e75a4a308a
Nicholas Chun Markus <nicxzmiller2@gmail.com>
2018-10-21 23:39:06 +0200

    login page

--- index.php
+++ index.php
@@ -4,12 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.css" media="screen" title="no title" charset="utf-8">
+    <title>Simple CMS</title>
 </head>
 <body>
     <div class="container">
         <h2>Login Page</h2>
-        <form action=""></form>
+        <form action="process_login.php" class="" method="post">
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" class="form-control">
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-outline-primary" value="Login">
+            </div>
+        </form>
     </div>
 </body>
 </html>
